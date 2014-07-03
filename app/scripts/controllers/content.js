d0b5f85e0020d3d752e156f0ae0eccb79e1e0671
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-07-03 20:34:56 +0530

    clean up code

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -2,26 +2,19 @@
 
 /**
  * @ngdoc function
- * @name ophioFoodly.controller:HealthyCtrl
+ * @name ophioFoodly.controller:ContentCtrl
  * @description
- * # HealthyCtrl
+ * # ContentCtrl
  * Controller of the ophioFoodly
  */
-app.controller('headCtrl', function ($scope, $location, ItemsStore, Users, Tabs) {
-    $scope.counter=0; 
+app.controller('ContentCtrl', function ($scope, $location, ItemsStore, Users, Tabs) {
     $scope.showIt = false;
     $scope.orderbyvote = 'upvotes';
     $scope.Items = ItemsStore;
     $scope.user = Users[1];
     $scope.currentTab = null;
 
-    var title = $location.path().substring(1);
-    if(title=="healthy")
-        $scope.currentTab = Tabs[0];
-    else if(title=="snacks")
-        $scope.currentTab = Tabs[1];
-    else if(title=="drinks")
-        $scope.currentTab = Tabs[2];
+    setCurrentTab();
 
     $scope.show = function(){
       $scope.showIt = true;
@@ -33,14 +26,28 @@ app.controller('headCtrl', function ($scope, $location, ItemsStore, Users, Tabs)
             item.upvotes++;
             $scope.user[cat] = true;
         }
-        
+
     };
 
     $scope.newItem = function(itemName){
         $scope.counter++;
         var cat = $scope.currentTab['title'];
-        $scope.Items.push(  { id: $scope.counter, name : itemName , upvotes : 0 , category : cat   } );
+        $scope.Items.push(  { id: getId($scope.Items), name : itemName , upvotes : 0 , category : cat   } );
         $scope.showIt = false;
         $scope.itemName = '';
-    };    
+    };
+
+    function getId(array){
+        return array.length;
+    }
+
+    function setCurrentTab(){
+        var title = $location.path().substring(1);
+        if(title=="healthy")
+            $scope.currentTab = Tabs[0];
+        else if(title=="snacks")
+            $scope.currentTab = Tabs[1];
+        else if(title=="drinks")
+            $scope.currentTab = Tabs[2];
+    }
   });
