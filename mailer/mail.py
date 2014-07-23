ac841ba3a3c2b6fa943333028af06b9cd3ad04de
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-07-23 14:57:19 +0530

    feature(scheduler): add scheduler in mailer

--- mailer/mail.py
+++ mailer/mail.py
@@ -1,40 +1,49 @@
 #! /usr/bin/env python
 # -*- coding : utf-8 -*-
 
+import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from mako.template import Template
 
+from settings import SUBJECT
 from settings import FROM
-from settings import Top
+from settings import TO
 from settings import SMTP_USER
 from settings import SMTP_PASSWORD
 
+
 def sendMail(data):
-    # Create message container - the correct MIME type is multipart/alternative.
-    msg = MIMEMultipart('alternative')
-
-    msg['Subject'] = "Top Eateries at Foodly"
-    msg['From'] = "sunny@ophio.co.in"
-    msg['To'] = "akshay@ophio.co.in"
-
-    # Create the body of the message (an HTML version).
-    mytemplate = Template(filename='template.html', module_directory='/tmp/mako_modules')
-    html = mytemplate.render(data=data)
-
-    # Record the MIME types of text/html.
-    part = MIMEText(html, 'html')
-
-    # Attach part into message container.
-    msg.attach(part)
+    # Set Logger Object
+    logger = logging.getLogger(LOGGER_NAME)
 
     try:
-        # Or port 465 doesn't seem to work!
+        # Create message container - the correct MIME type is
+        # multipart/alternative.
+        msg = MIMEMultipart('alternative')
+
+        msg['Subject'] = SUBJECT
+        msg['From'] = FROM
+        msg['To'] = TO
+
+        # Create the body of the message (an HTML version).
+        mytemplate = Template(filename='template.html')
+        html = mytemplate.render(data=data)
+
+        # Record the MIME types of text/html.
+        part = MIMEText(html, 'html')
+
+        # Attach part into message container.
+        msg.attach(part)
+
+        # Set SMTP server login ceredentials and send mail
         server = smtplib.SMTP('smtp.sendgrid.net')
         server.starttls()
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.sendmail(FROM, TO, msg.as_string())
         server.quit()
-        print 'Successfully sent the mail'
+        logger.info('Successfully sent the mail')
     except Exception as e:
-        print "Failed to send mail :: ", str(e)
+        logger.exception("Failed to send mail")
